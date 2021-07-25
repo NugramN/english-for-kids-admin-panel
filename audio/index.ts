b7d03ebd2e7888c9ff8b54a9f63059becf3d3ee0
Anastasia Marhun <anastasiamarhun@gmail.com>
2021-07-25 12:33:47 +0300

    init: add initial files

---
+++ audio/index.ts
@@ -0,0 +1,80 @@
+import angryAudio from './angry.mp3';
+import openAudio from './open.mp3';
+import cryAudio from './cry.mp3';
+import catAudio from './cat.mp3';
+import birdAudio from './bird.mp3';
+import skirtAudio from './skirt.mp3';
+import sadAudio from './sad.mp3';
+import dogAudio from './dog.mp3';
+import blouseAudio from './blouse.mp3';
+import bootAudio from './boot.mp3';
+import chickAudio from './chick.mp3';
+import chickenAudio from './chicken.mp3';
+import coatAudio from './coat.mp3';
+import danceAudio from './dance.mp3';
+import diveAudio from './dive.mp3';
+import dolphinAudio from './dolphin.mp3';
+import drawAudio from './draw.mp3';
+import dressAudio from './dress.mp3';
+import fishAudio from './fish.mp3';
+import failureAudio from './failure.mp3';
+import flyAudio from './fly.mp3';
+import frogAudio from './frog.mp3';
+import giraffeAudio from './giraffe.mp3';
+import happyAudio from './happy.mp3';
+import horseAudio from './horse.mp3';
+import hugAudio from './hug.mp3';
+import jumpAudio from './jump.mp3';
+import laughAudio from './laugh.mp3';
+import lionAudio from './lion.mp3';
+import mouseAudio from './mouse.mp3';
+import pantsAudio from './pants.mp3';
+import pigAudio from './pig.mp3';
+import playAudio from './play.mp3';
+import pointAudio from './point.mp3';
+import rabbitAudio from './rabbit.mp3';
+import rideAudio from './ride.mp3';
+import runAudio from './run.mp3';
+import scaredAudio from './scared.mp3';
+import sheepAudio from './sheep.mp3';
+import shirtAudio from './shirt.mp3';
+import shoeAudio from './shoe.mp3';
+import singAudio from './sing.mp3';
+import skipAudio from './skip.mp3';
+import smileAudio from './smile.mp3';
+import surprisedAudio from './surprised.mp3';
+import successAudio from './success.mp3';
+import swimAudio from './swim.mp3';
+import tiredAudio from './tired.mp3';
+import turtleAudio from './turtle.mp3';
+import cheeseAudio from './cheese.mp3';
+import cookieAudio from './cookie.mp3';
+import hamburgerAudio from './hamburger.mp3';
+import noodlesAudio from './noodles.mp3';
+import pancakeAudio from './pancake.mp3';
+import pizzaAudio from './pizza.mp3';
+import saladAudio from './salad.mp3';
+import tacoAudio from './taco.mp3';
+import beachAudio from './beach.mp3';
+import bridgeAudio from './bridge.mp3';
+import cityAudio from './city.mp3';
+import desertAudio from './desert.mp3';
+import farmAudio from './farm.mp3';
+import forestAudio from './forest.mp3';
+import officeAudio from './office.mp3';
+import parkAudio from './park.mp3';
+import correctAudio from './correct.mp3';
+import errorAudio from './error.mp3';
+import donutAudio from './donut.mp3';
+
+export {
+  angryAudio, openAudio, cryAudio, birdAudio, skirtAudio, sadAudio, catAudio, dogAudio, blouseAudio
+  , bootAudio, chickAudio, chickenAudio, coatAudio, danceAudio, diveAudio, dolphinAudio, drawAudio
+  , dressAudio, fishAudio, failureAudio, flyAudio, frogAudio, giraffeAudio, happyAudio
+  , horseAudio, hugAudio, jumpAudio, laughAudio, lionAudio, mouseAudio, pantsAudio, pigAudio
+  , playAudio, pointAudio, rabbitAudio, rideAudio, runAudio, scaredAudio, sheepAudio, shirtAudio
+  , shoeAudio, singAudio, skipAudio, smileAudio, surprisedAudio, swimAudio, successAudio
+  , tiredAudio, turtleAudio, cheeseAudio, cookieAudio, hamburgerAudio, noodlesAudio, donutAudio
+  , pancakeAudio, pizzaAudio, saladAudio, tacoAudio, beachAudio, bridgeAudio, cityAudio,
+  desertAudio, farmAudio, forestAudio, officeAudio, parkAudio, correctAudio, errorAudio,
+};
---
+++ img/index.ts
@@ -0,0 +1,83 @@
+import angryPic from './angry.jpg';
+import openPic from './open.jpg';
+import cryPic from './cry.jpg';
+import catPic from './cat.jpg';
+import birdPic from './bird.jpg';
+import skirtPic from './skirt.jpg';
+import sadPic from './sad.jpg';
+import dogPic from './dog.jpg';
+import blousePic from './blouse.jpg';
+import bootPic from './boot.jpg';
+import chickPic from './chick.jpg';
+import chickenPic from './chicken.jpg';
+import coatPic from './coat.jpg';
+import dancePic from './dance.jpg';
+import divePic from './dive.jpg';
+import dolphinPic from './dolphin.jpg';
+import drawPic from './draw.jpg';
+import dressPic from './dress.jpg';
+import fishPic from './fish.jpg';
+import fish1Pic from './fish1.jpg';
+import flyPic from './fly.jpg';
+import frogPic from './frog.jpg';
+import giraffePic from './giraffe.jpg';
+import happyPic from './happy.jpg';
+import horsePic from './horse.jpg';
+import hugPic from './hug.jpg';
+import jumpPic from './jump.jpg';
+import laughPic from './laugh.jpg';
+import lionPic from './lion.jpg';
+import mousePic from './mouse.jpg';
+import pantsPic from './pants.jpg';
+import pigPic from './pig.jpg';
+import playPic from './play.jpg';
+import pointPic from './point.jpg';
+import rabbitPic from './rabbit.jpg';
+import ridePic from './ride.jpg';
+import runPic from './run.jpg';
+import scaredPic from './scared.jpg';
+import sheepPic from './sheep.jpg';
+import shirtPic from './shirt.jpg';
+import shoePic from './shoe.jpg';
+import singPic from './sing.jpg';
+import skipPic from './skip.jpg';
+import smilePic from './smile.jpg';
+import surprisedPic from './surprised.jpg';
+import swimPic from './swim.jpg';
+import tiredPic from './tired.jpg';
+import turtlePic from './turtle.jpg';
+import cheesePic from './cheese.jpg';
+import cookiePic from './cookie.jpg';
+import hamburgerPic from './hamburger.jpg';
+import noodlesPic from './noodles.jpg';
+import pancakePic from './pancake.jpg';
+import pizzaPic from './pizza.jpg';
+import saladPic from './salad.jpg';
+import tacoPic from './taco.jpg';
+import donutPic from './donut.jpg';
+import beachPic from './beach.jpg';
+import bridgePic from './bridge.jpg';
+import cityPic from './city.jpg';
+import desertPic from './desert.jpg';
+import farmPic from './farm.jpg';
+import forestPic from './forest.jpg';
+import officePic from './office.jpg';
+import parkPic from './park.jpg';
+import dancingePic from './dancing.jpg';
+import failurePic from './failure.jpg';
+import emptyHeartPic from './empty-heart.jpg';
+import heartPic from './heart.jpg';
+import loopPic from './loop.jpg';
+import shopPic from './shop.png';
+import hotdogPic from './hotdog.png';
+
+export {
+  angryPic, openPic, cryPic, birdPic, skirtPic, sadPic, catPic, dogPic, blousePic, bootPic
+  , chickPic, chickenPic, coatPic, dancePic, divePic, dolphinPic, drawPic, dressPic, fishPic
+  , failurePic, fish1Pic, flyPic, frogPic, giraffePic, happyPic, horsePic, hugPic, jumpPic
+  , laughPic, lionPic, mousePic, pantsPic, pigPic, playPic, pointPic, rabbitPic, ridePic, runPic
+  , scaredPic, sheepPic, shirtPic, shoePic, singPic, skipPic, smilePic, surprisedPic, swimPic,
+  tiredPic, turtlePic, cheesePic, cookiePic, hamburgerPic, noodlesPic, loopPic, shopPic
+  , pancakePic, pizzaPic, saladPic, tacoPic, beachPic, bridgePic, cityPic, desertPic, donutPic,
+  farmPic, forestPic, officePic, parkPic, dancingePic, emptyHeartPic, heartPic, hotdogPic,
+};
